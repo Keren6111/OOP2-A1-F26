723372ab61becd5fd3e338e485791cda3d18547d
Keren <kerenaldavida.chakou@crc-lennox.qc.ca>
2026-09-24 07:05:23 -0400

    Person's class and restrictions added

---
+++ src/main/java/org/champlain/oop2/oop2a1f26/Person.java
@@ -0,0 +1,52 @@
+package org.champlain.oop2.oop2a1f26;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private String aName;
+    private LocalDate aDOB;
+    private String aEmailAddress;
+    private boolean aPurchasedParkingPass;
+
+    public Person(String pName, LocalDate pDOB, String pEmailAddress) {
+        this.aName = pName;
+        this.aDOB = pDOB;
+        this.aEmailAddress = pEmailAddress;
+        this.aPurchasedParkingPass = false;
+    }
+
+    public String getName() {
+        return this.aName;
+    }
+
+    public LocalDate getDOB() {
+        return this.aDOB;
+    }
+
+    public String getEmailAddress() {
+        return this.aEmailAddress;
+    }
+
+    public boolean isPurchasedParkingPass() {
+        return this.aPurchasedParkingPass;
+    }
+
+    public boolean purchaseParkingPass() {
+        if (this.aPurchasedParkingPass) {
+            return false;
+        }
+
+        this.aPurchasedParkingPass = true;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{name='" + this.aName
+                + "', DOB=" + this.aDOB
+                + ", email='" + this.aEmailAddress
+                + "', parkingPass=" + this.aPurchasedParkingPass
+                + "}";
+    }
+}
